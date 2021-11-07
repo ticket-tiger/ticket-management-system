474830fe3f2b32ea39b6d5c467a6cde158182738
K-Blask <blasco003@wcsu.edu>
2021-11-07 18:38:00 -0500

    FormUpdate

--- src/components/SubmitTicket/SubmitTicket.js
+++ src/components/SubmitTicket/SubmitTicket.js
@@ -1,44 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './SubmitTicket.css';
-import TicketLog from '../TicketLog/TicketLog';
+// import TicketLog from '../TicketLog/TicketLog';
 
-const SubmitTicket = () => 
-{
-   const[ticketText, setTicketText] = useState('');
-   
-   const clickHandler =() => 
-   {
-      
-      console.log('Clicked')
-      axios.post('http://localhost:3001/api/create-ticket', ticketText,
+const SubmitTicket = () => {
+  const [ticketText, setTicketText] = useState('');
+
+  const clickHandler = () => {
+    console.log('Clicked');
+    axios.post('http://localhost:3001/api/create-ticket', ticketText,
       {
-       })
-       .catch(function (error) {
-         console.log(error);
-       });
-   };
-   const clickHandler2 = () =>
-   {
-      
-   }
-   return (
-      <div className = 'submit-ticket'>
-            <form>What is the issue you are currently having?
-               <label>
-                  <textarea type = "text" onInput = {e => setTicketText(e.target.value)}/>
-               </label>
-            </form>
-            <div>
-               <button onClick = {clickHandler2}>
-                  Cancel
-               </button>
-               <button
-                  //className = 'submit-ticket__button' 
-                  onClick ={{clickHandler}}>
-                  Submit
-               </button>
-            </div>
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const clickHandler2 = () => {
+
+  };
+  return (
+    <div className="submit-ticket">
+      <form>
+        What is the issue you are currently having?
+        <label>
+          <textarea type="text" rows="10" cols="25" onInput={(e) => setTicketText(e.target.value)} />
+        </label>
+      </form>
+      <div>
+        <button type="submit" onClick={clickHandler2}>
+          Cancel
+        </button>
+        <button
+          type="submit"
+                  // className = 'submit-ticket__button'
+          onClick={{ clickHandler }}
+        >
+          Submit
+        </button>
       </div>
-   );
-}
+    </div>
+  );
+};
 export default SubmitTicket;
--- src/components/TicketLog/TicketLog.js
+++ src/components/TicketLog/TicketLog.js
@@ -1,25 +1,21 @@
-import React,{useState, useEffect} from 'react';
-import axios from 'axios';
+import React, { useEffect } from 'react';
+// import axios from 'axios';
 import './TicketLog.css';
-import SubmitTicket from '../SubmitTicket/SubmitTicket';
+// import SubmitTicket from '../SubmitTicket/SubmitTicket';
 
 const TicketLog = () => {
-   
-   useEffect(() => {
-   //   getTickets()
-   },[])
-   
-   const sendTicketText = async () => {
-      const response = await axios.get('http://localhost:3001/api/get-tickets',
-   {
-      
-   })
-   };
-   return
-   (
-   //This function is waiting for the server to send ticket data from the server
+  useEffect(() => {
+    //   getTickets()
+  }, []);
 
-      <div/>
-   );
-}
+  //   const sendTicketText = async () => {
+  //     const response = await axios.get('http://localhost:3001/api/get-tickets',{
+
+  //       });
+  //  };
+  return (
+  // This function is waiting for the server to send ticket data from the server
+    <div />
+  );
+};
 export default TicketLog;
