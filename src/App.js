1f651653b56b7baf6cd19b56950b865839ae98d3
Shelby-Simpson <shelbysimpson98@gmail.com>
2021-11-07 13:39:47 -0500

    Installed eslint and set up eslint config file .eslintrc.json.  PLS INSTALL ESLINT EXT ON YOUR VSCODE

--- src/App.js
+++ src/App.js
@@ -6,11 +6,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
-      <SubmitTicket/>
+      <header className="App-header" />
+      <SubmitTicket />
       <p>Test</p>
-      </div>
+    </div>
   );
 }
 
--- src/components/SubmitTicket/SubmitTicket.js
+++ src/components/SubmitTicket/SubmitTicket.js
@@ -1,42 +1,41 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import './SubmitTicket.css';
 
-const SubmitTicket = () => 
-{
-   // const sendTicketText = async () =>
-   // {
-   //    const response = await axios.get('http://localhost:3001/api/get-tickets',
-   //    {
-         
-   //    })
-   // };
-   const[ticketText, setTicketText] = useState('');
-   
-   const clickHandler =() => 
-   {
-      
-      console.log('Clicked')
-      axios.post('http://localhost:3001/api/create-ticket', ticketText,
+const SubmitTicket = () => {
+  // const sendTicketText = async () =>
+  // {
+  //    const response = await axios.get('http://localhost:3001/api/get-tickets',
+  //    {
+
+  //    })
+  // };
+  const [ticketText, setTicketText] = useState('');
+
+  const clickHandler = () => {
+    console.log('Clicked');
+    axios.post('http://localhost:3001/api/create-ticket', ticketText,
       {
-       })
-       .catch(function (error) {
-         console.log(error);
-       });
-   };
-   return (
-      <div className = 'submit-ticket'>
-            <form>What is the issue you are currently having?
-               <label>
-                  <textarea type = "text" onInput = {e => setTicketText(e.target.value)}/>
-               </label>
-            </form>
-            <button
-               //className = 'submit-ticket__button' 
-               onClick ={{clickHandler}}>
-               Submit
-            </button>
-      </div>
-   );
-}
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <div className="submit-ticket">
+      <form>
+        What is the issue you are currently having?
+        <label>
+          <textarea type="text" onInput={(e) => setTicketText(e.target.value)} />
+        </label>
+      </form>
+      <button
+               // className = 'submit-ticket__button'
+        onClick={{ clickHandler }}
+      >
+        Submit
+      </button>
+    </div>
+  );
+};
 export default SubmitTicket;
