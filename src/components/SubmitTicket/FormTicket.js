07b0f115296885a01ccd36a5d9e3404eefc6daab
axioms123 <61398302+axioms123@users.noreply.github.com>
2021-11-07 16:51:04 -0500

    style

--- src/components/SubmitTicket/FormTicket.js
+++ src/components/SubmitTicket/FormTicket.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import './FormTicket';
+import './FormTicket.css';
 
 function FormTicket() {
   return (
     <form>
       <label>
-        <textarea type= "text" rows="10" cols="25" />
+        <textarea type="text" rows="10" cols="25" />
       </label>
     </form>
-  )
+  );
 }
-//ReactDOM.render(<FormTicket/>, document.getElementById('root'));
+// ReactDOM.render(<FormTicket/>, document.getElementById('root'));
 export default FormTicket;
--- src/components/SubmitTicket/SubmitTicket.js
+++ src/components/SubmitTicket/SubmitTicket.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import './SubmitTicket.css';
 import FormTicket from './FormTicket';
-const SubmitTicket = () => 
-{
-   const clickHandler =() => 
-   {
-      console.log('Clicked')
-   };
-   return (
-      <div class = 'submit-ticket'>
-         <div class = 'title'>Describe your issue </div>
-         <FormTicket/>
-         <button
-            class = 'submit-ticket__button' 
-            onClick ={{clickHandler}}>
-            Submit
-         </button>
-         
-      </div>
-   );
-}
+
+const SubmitTicket = () => {
+  const clickHandler = () => {
+    console.log('Clicked');
+  };
+  return (
+    <div className="submit-ticket">
+      <div className="title">Describe your issue </div>
+      <FormTicket />
+      <button type="submit" className="submit-ticket__button" onClick={{ clickHandler }}>
+        Submit
+      </button>
+
+    </div>
+  );
+};
 export default SubmitTicket;
