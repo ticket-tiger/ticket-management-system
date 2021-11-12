83f91f661ccbe01fbacf15f47818b8dd6a024358
K-Blask <blasco003@wcsu.edu>
2021-11-12 17:19:11 -0500

    text description

--- src/components/SubmitTicket/SubmitTicket.js
+++ src/components/SubmitTicket/SubmitTicket.js
@@ -3,17 +3,18 @@ import axios from 'axios';
 import './SubmitTicket.css';
 
 const SubmitTicket = () => {
-  // const [ticketText, setTicketText] = useState('');
+  const [ticketText, setTicketText] = useState('');
   const [responseStatus, setResponseStatus] = useState(null);
 
   const sendPostRequest = async () => {
     const ticket = {
       category: 'category1',
       title: 'test1',
-      description: 'The login button doesn\x27t work.',
+      description: ticketText,
       priority: 'Low',
       urgency: 'High',
     };
+    setTicketText('');
     console.log('Ticket sent');
     const response = await axios.post('http://localhost:3001/api/create-ticket', ticket);
     return response.status;
@@ -35,7 +36,7 @@ const SubmitTicket = () => {
           <tr>
             <th>What is the issue you are currently having?</th>
             <td>
-              {/* <textarea type="text" onInput={(e) => setTicketText(e.target.value)} /> */}
+              <textarea type="text" onInput={(e) => setTicketText(e.target.value)} value={ticketText} />
             </td>
           </tr>
         </thead>
