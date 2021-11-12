d1cacf4c6b7b553b06803253851f2c77359540bb
K-Blask <blasco003@wcsu.edu>
2021-11-12 13:32:43 -0500

    update

--- src/tickets/routes.js
+++ src/tickets/routes.js
@@ -1,21 +1,22 @@
 import express from 'express';
-import { createTicket } from '../db.js';
+import { createTicket, getTickets } from '../db.js';
 
 const router = express.Router();
 
 router.get('/get-tickets', async (req, res) => {
   console.log('Received GET request.');
-  
+  const result = await getTickets();
+  res.send(result);
   res.end();
 });
 
-router.post('/create-ticket', (req, res) => {
-   console.log('Received POST request.');
-   const ticket = {
-   title: req.body.title,
-   description: req.body.description,
-   priority: req.body.priority,
-   date: new Date().toLocaleString('en-US', { timeZone: 'America/New_York' }),
+router.post('/create-ticket', async (req, res) => {
+  console.log('Received POST request.');
+  const ticket = {
+    title: req.body.title,
+    description: req.body.description,
+    priority: req.body.priority,
+    date: new Date().toLocaleString('en-US', { timeZone: 'America/New_York' }),
   };
   const result = await createTicket(ticket);
   res.send(result);
