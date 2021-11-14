632573edd8ed5c653ac35dbc8f044cb798eec909
K-Blask <blasco003@wcsu.edu>
2021-11-14 18:43:55 -0500

    added await, ticket collection is now toArray

--- src/db.js
+++ src/db.js
@@ -23,7 +23,7 @@ export const getTicketCollection = async () => {
   try {
     await client.connect();
 
-    const result = client.db(config.mongodb.database).collection('tickets').find();
+    const result = await client.db(config.mongodb.database).collection('tickets').find().toArray();
 
     return result;
   } finally {
@@ -55,37 +55,37 @@ const deleteAllTickets = async () => {
   }
 };
 
-// const tickets = [
-//   {
-//     category: 'category1',
-//     title: 'test1',
-//     description: 'The login button doesn\x27t work.',
-//     priority: 'Low',
-//     urgency: 'High',
-//     dateCreated: new Date().toLocaleString('en-US', { timeZone: 'America/New_York' }),
-//   },
-//   {
-//     category: 'category2',
-//     title: 'test2',
-//     description: 'The screen freezes when I run a file called malware.exe.',
-//     priority: 'Medium',
-//     urgency: 'Medium',
-//     dateCreated: new Date().toLocaleString('en-US', { timeZone: 'America/New_York' }),
-//   },
-//   {
-//     category: 'category3',
-//     title: 'test3',
-//     description: 'I received bad customer service the last time I called you.',
-//     priority: 'High',
-//     urgency: 'Low',
-//     dateCreated: new Date().toLocaleString('en-US', { timeZone: 'America/New_York' }),
-//   }];
+const tickets = [
+  {
+    category: 'category1',
+    title: 'test1',
+    description: 'The login button doesn\x27t work.',
+    priority: 'Low',
+    urgency: 'High',
+    dateCreated: new Date().toLocaleString('en-US', { timeZone: 'America/New_York' }),
+  },
+  {
+    category: 'category2',
+    title: 'test2',
+    description: 'The screen freezes when I run a file called malware.exe.',
+    priority: 'Medium',
+    urgency: 'Medium',
+    dateCreated: new Date().toLocaleString('en-US', { timeZone: 'America/New_York' }),
+  },
+  {
+    category: 'category3',
+    title: 'test3',
+    description: 'I received bad customer service the last time I called you.',
+    priority: 'High',
+    urgency: 'Low',
+    dateCreated: new Date().toLocaleString('en-US', { timeZone: 'America/New_York' }),
+  }];
 
-// const testDB = async () => {
-//   await deleteAllTickets().catch(console.dir);
-//   await createTickets(tickets).catch(console.dir);
-//   const returnedTickets = await getTicketCollection().catch(console.dir);
-//   console.log(returnedTickets);
-// };
+const testDB = async () => {
+  await deleteAllTickets().catch(console.dir);
+  await createTickets(tickets).catch(console.dir);
+  const returnedTickets = await getTicketCollection().catch(console.dir);
+  console.log(returnedTickets);
+};
 
-// testDB();
+testDB();
