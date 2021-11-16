0700c6f1dd18186e7c69b6797e89f9411261d055
Shelby-Simpson <shelbysimpson98@gmail.com>
2021-11-15 21:02:11 -0500

    Added CreateAccount, Login, and LandingPage components and install react router

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './LandingPage.css';
+
+const LandingPage = () => (
+  <div>
+    <button type="button">
+      Create Account
+    </button>
+    <button type="button">
+      Login
+    </button>
+    <button type="button">
+      Continue As Guest
+    </button>
+  </div>
+);
+
+export default LandingPage;
