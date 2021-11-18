e03b30cc32cbc7dbda375b3403c6edd141960c20
Shelby-Simpson <shelbysimpson98@gmail.com>
2021-11-18 07:22:12 -0500

    Added working buttons on landing page that link to proper pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import SubmitTicket from './components/SubmitTicket/SubmitTicket';
 import TicketLog from './components/TicketLog/TicketLog';
+import LandingPage from './components/LandingPage/LandingPage';
+import CreateAccount from './components/CreateAccount/CreateAccount';
+import Login from './components/Login/Login';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <div className="submit-ticket-container">
-        <h1 className="submit-ticket-heading">Have an issue? Let us know.</h1>
-        {/* <header className="App-header" /> */}
-        <SubmitTicket />
-      </div>
-      <h3 className="ticket-log-heading">See our existing issues below...</h3>
-      <TicketLog />
+      <Routes>
+        <Route exact path="/" element={<LandingPage />} />
+        <Route path="/create-account" element={<CreateAccount />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/user" element={<SubmitTicket />} />
+        <Route path="/employee" element={<TicketLog />} />
+        {/* <div>
+        <LandingPage />
+        <div className="submit-ticket-container">
+          <h1 className="submit-ticket-heading">Have an issue? Let us know.</h1>
+          <SubmitTicket />
+        </div>
+        <h3 className="ticket-log-heading">See our existing issues below...</h3>
+        <TicketLog />
+      </div> */}
+      </Routes>
     </div>
   );
 }
--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './LandingPage.css';
 
 const LandingPage = () => (
   <div>
     <button type="button">
-      Create Account
+      <Link to="/create-account">
+        Create Account
+      </Link>
     </button>
     <button type="button">
-      Login
+      <Link to="/login">
+        Login
+      </Link>
     </button>
     <button type="button">
-      Continue As Guest
+      <Link to="/user">
+        Continue As Guest
+      </Link>
+    </button>
+    <button type="button">
+      <Link to="/employee">
+        Continue As Employee
+      </Link>
     </button>
   </div>
 );
